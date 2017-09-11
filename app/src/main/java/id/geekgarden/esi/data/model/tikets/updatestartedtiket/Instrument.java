b9a38f97e0d9f76ebfee24ab4463c468b6820ebe
Indra Setiawan <indr497@gmail.com>
2017-09-11 17:46:58 +0700

    menambahkan fungsi start tiket

---
+++ app/src/main/java/id/geekgarden/esi/data/model/tikets/updatestartedtiket/Instrument.java
@@ -0,0 +1,21 @@
+
+package id.geekgarden.esi.data.model.tikets.updatestartedtiket;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Instrument {
+
+    @SerializedName("data")
+    @Expose
+    private Data__ data;
+
+    public Data__ getData() {
+        return data;
+    }
+
+    public void setData(Data__ data) {
+        this.data = data;
+    }
+
+}
