65766a894859347aa46c0550761e9f4b9e417582
raka <jamban201>
2017-11-30 20:19:00 +0700

    add installation IT

---
+++ app/src/main/java/id/geekgarden/esi/listtiket/activityticketstaff/DetailOnProgressPmIt.java
@@ -0,0 +1,40 @@
+package id.geekgarden.esi.listtiket.activityticketstaff;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import id.geekgarden.esi.R;
+
+public class DetailOnProgressPmIt extends AppCompatActivity {
+
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_on_pm_it);
+        ButterKnife.bind(this);
+        initActionbar();
+    }
+
+    private void initActionbar() {
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Detail Dialihkan");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
